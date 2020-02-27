43ba179cb70467d86b2adc7aeab59329d2856de4
gisvignesh <vignesh.k@greatinnovus.com>
2020-02-27 17:16:09 +0530

    FInal code

--- src/app/user/user/user.component.ts
+++ src/app/user/user/user.component.ts
@@ -3,14 +3,14 @@ import { UserService } from '../../services/user.service';
 import { ThrowStmt } from '@angular/compiler';
 
 @Component({
-  selector: 'app-user',
-  templateUrl: './user.component.html',
-  styleUrls: ['./user.component.scss']
+	selector: 'app-user',
+	templateUrl: './user.component.html',
+	styleUrls: ['./user.component.scss']
 })
 export class UserComponent implements OnInit {
-	userList :any;
+	userList: any;
 	userDataList = [];
-	userRepoList : any;
+	userRepoList: any;
 	page;
 	repoList = false;
 	userName;
@@ -19,52 +19,64 @@ export class UserComponent implements OnInit {
 	scrollDistance = 1;
 	scrollUpDistance = 2;
 	direction = '';
-	searchKey ;
-	constructor(private userService : UserService) { }
+	searchKey;
+	constructor(private userService: UserService) { }
 
 	ngOnInit() {
 		this.page = 0;
 		this.getUsers()
 	}
 
-	getUsers(){
+	getUsers() {
 		// getUserList
-		let count = this.page * 15;
-		let query = '?since='+count;
-		
-		this.userService.getUserList(query).subscribe(resp=>{
-			this.page ? resp.forEach(item=>{this.userList.push(item)}) : (this.userList = resp ? resp : [])
-				console.log(this.userList);
-				this.userList.forEach(item=>{
-					this.getUserData(item.login)
-				})
-				
+		let count = this.page * 5;
+		let query = '?since=' + count;
+
+		this.userService.getUserList(query).subscribe(resp => {
+			this.page ? resp.forEach(item => { this.userList.push(item) }) : (this.userList = resp ? resp : [])
+			console.log(this.userList);
+			this.userList.forEach(item => {
+				this.getUserData(item.login)
+			})
+
 		})
 	}
 
-	getUserRepos(userName){
+	getUserRepos(userName) {
 		let query = '';
 		this.userName = userName;
 		this.repoList == false ? this.page = 1 : '';
 		this.repoList = true;
-		this.userService.getUserRepoList(query,userName).subscribe(response=>{
+		this.userService.getUserRepoList(query, userName).subscribe(response => {
 			console.log(response);
 			this.userRepoList = response ? response : [];
 		})
 	}
 
-	searchUser(key){
-		let query = '?q='+key+'&page='+this.page+'&per_page=10';
-		this.searchKey = key;
-		this.userService.getSearchUserList(query).subscribe(response=>{
-			this.userList = response && response.items ? response.items : [];
-		})
+	searchUser(key) {
+		if(key){
+			let query = '?q=' + key + '&page=' + this.page + '&per_page=5';
+			this.searchKey = key;
+			this.userList = [];
+			this.userDataList = [];
+			this.userService.getSearchUserList(query).subscribe(response => {
+				this.userList = response && response.items ? response.items : [];
+				this.userList.forEach(item => {
+					this.getUserData(item.login)
+				})
+			})
+		}
+		else{
+			this.searchKey = '';
+			this.getUsers()
+		}
+		
 	}
 
-	getUserData(name){
+	getUserData(name) {
 		let data;
 		let query = '';
-		this.userService.getUserData(query,name).subscribe(response=>{
+		this.userService.getUserData(query, name).subscribe(response => {
 			data = response;
 			this.userDataList.push(data)
 		})
@@ -75,7 +87,7 @@ export class UserComponent implements OnInit {
 		this.searchKey ? this.searchUser(this.searchKey) : this.getUsers();
 	}
 
-	back(){
+	back() {
 		this.repoList = false;
 		this.page = 1;
 		this.getUsers();
